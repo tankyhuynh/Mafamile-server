f55b6ef69fd0ddc33651fbbc23d07fc47ac5de2d
TanKy <pokijellyperiod1@gmail.com>
2020-09-11 10:14:39 +0700

    asd

--- src/main/java/com/mafami/Mafami/MafamiApplication.java
+++ src/main/java/com/mafami/Mafami/MafamiApplication.java
@@ -2,10 +2,14 @@ package com.mafami.Mafami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin	
+@ComponentScan(basePackages = "com.mafami.Mafami")
+@EnableMongoRepositories(basePackages = "com.mafami.Mafami.Repository")
+@CrossOrigin(origins = "http://localhost:8081")
 public class MafamiApplication{
 																																																																																																																																																																																																																																																																																																																																																																																																																																																	
 	public static void main(String[] args) {	
