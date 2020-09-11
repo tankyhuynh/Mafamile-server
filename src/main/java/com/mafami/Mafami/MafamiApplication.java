3e15efa715640cc06601656152110f15b07d39ff
TanKy <pokijellyperiod1@gmail.com>
2020-09-11 10:04:54 +0700

    asd

--- src/main/java/com/mafami/Mafami/MafamiApplication.java
+++ src/main/java/com/mafami/Mafami/MafamiApplication.java
@@ -2,8 +2,10 @@ package com.mafami.Mafami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin	
 public class MafamiApplication{
 																																																																																																																																																																																																																																																																																																																																																																																																																																																	
 	public static void main(String[] args) {	
