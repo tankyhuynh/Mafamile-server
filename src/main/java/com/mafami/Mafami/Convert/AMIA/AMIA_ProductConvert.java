2edfc4d92a49f7230a302ae5141138cc8a115748
TanKy <pokijellyperiod1@gmail.com>
2020-11-04 09:52:35 +0700

    Modify price menu

--- src/main/java/com/mafami/Mafami/Convert/AMIA/AMIA_ProductConvert.java
+++ src/main/java/com/mafami/Mafami/Convert/AMIA/AMIA_ProductConvert.java
@@ -10,7 +10,7 @@ public class AMIA_ProductConvert {
 	public AMIA_MenuEntity entity_to_entity(AMIA_MenuEntity entity1) {
 		AMIA_MenuEntity entity2 = new AMIA_MenuEntity();
 		entity2.setName(entity1.getName());
-		entity2.setPrice(entity2.getPrice());
+		entity2.setPrice(entity1.getPrice());
 		entity2.setCategory(entity1.getCategory());
 		entity2.setImage(entity1.getImage());
 		entity2.setSite(entity1.getSite());
