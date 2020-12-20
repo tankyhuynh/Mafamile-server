3c37ee300a261a8da50992fb014dfe64136b1466
tankyhuynh <pokijellyperiod1@gmail.com>
2020-12-20 21:21:21 +0700

    Modify billAPI

--- src/main/java/com/mafami/Mafami/Entity/CategoryEntity.java
+++ src/main/java/com/mafami/Mafami/Entity/CategoryEntity.java
@@ -2,54 +2,22 @@ package com.mafami.Mafami.Entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document(collection = "amia_categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryEntity {
 
 	@Id
 	private String id;
-
 	private String name;
 	private String slug;
 	private String site;
 
-	public CategoryEntity() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSlug() {
-		return slug;
-	}
-
-	public void setSlug(String slug) {
-		this.slug = slug;
-	}
-
-	public String getSite() {
-		return site;
-	}
-
-	public void setSite(String site) {
-		this.site = site;
-	}
-	
-	
 
 }
--- src/main/java/com/mafami/Mafami/Entity/PostEntity.java
+++ src/main/java/com/mafami/Mafami/Entity/PostEntity.java
@@ -2,12 +2,18 @@ package com.mafami.Mafami.Entity;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document("ami_design_post")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostEntity {
 
 	@Id
@@ -20,82 +26,6 @@ public class PostEntity {
 	private String preview;
 	private String site;
 
-	public PostEntity() {
-		// TODO Auto-generated constructor stub
-	}
 
-	public PostEntity(String id, String title, String content, Date time, UserEntity author, String site) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.time = time;
-		this.author = author;
-		this.site = site;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Date getTime() {
-		return time;
-	}
-
-	public void setTime(Date time) {
-		this.time = time;
-	}
-
-	public UserEntity getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(UserEntity author) {
-		this.author = author;
-	}
-
-	public String getSite() {
-		return site;
-	}
-
-	public void setSite(String site) {
-		this.site = site;
-	}
-
-	public String getThumbnail() {
-		return thumbnail;
-	}
-
-	public void setThumbnail(String thumbnail) {
-		this.thumbnail = thumbnail;
-	}
-
-	public String getPreview() {
-		return preview;
-	}
-
-	public void setPreview(String preview) {
-		this.preview = preview;
-	}
 
 }
