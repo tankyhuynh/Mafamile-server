8c3ec887b9ce6962bfb9044733c0cb42a8c1c90e
tankyhuynh <pokijellyperiod1@gmail.com>
2020-12-26 12:00:10 +0700

    Modify content

--- src/main/java/com/mafami/Mafami/model/FoodInformationModel.java
+++ src/main/java/com/mafami/Mafami/model/FoodInformationModel.java
@@ -1,8 +1,10 @@
 package com.mafami.Mafami.model;
 
-import java.util.UUID;
-
 import com.mafami.Mafami.Entity.MenuEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
 * @author root {11:11:28 AM}:
@@ -14,54 +16,12 @@ import com.mafami.Mafami.Entity.MenuEntity;
  *
  */
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FoodInformationModel {
 
-	private String id = UUID.randomUUID().toString();
 	private MenuEntity food;
-	private String additionInformation;
 	private int quantity;
-
-	public FoodInformationModel() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public FoodInformationModel(String id, MenuEntity food, int quantity) {
-		super();
-		this.id = id;
-		this.food = food;
-		this.quantity = quantity;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public MenuEntity getFood() {
-		return food;
-	}
-
-	public void setFood(MenuEntity food) {
-		this.food = food;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public String getAdditionInformation() {
-		return additionInformation;
-	}
-
-	public void setAdditionInformation(String additionInformation) {
-		this.additionInformation = additionInformation;
-	}
 
 }
