7fd1270ae2c2ebd2bcdc61612a611d1c76c24fc0
tankyhuynh <pokijellyperiod1@gmail.com>
2021-01-02 12:05:28 +0700

    Search Bill

--- src/main/java/com/mafami/Mafami/Config/AsyncConfig.java
+++ src/main/java/com/mafami/Mafami/Config/AsyncConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@ComponentScan(basePackages = "com.mafami.Mafami")
+//@ComponentScan(basePackages = "com.mafami.Mafami")
 @EnableAsync
 public class AsyncConfig {
 
