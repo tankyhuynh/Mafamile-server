ffa4b6ca375ab9dc3226abfd661672c7e81180b6
tankyhuynh <pokijellyperiod1@gmail.com>
2021-01-02 12:12:15 +0700

    Search Bill

--- src/main/java/com/mafami/Mafami/api/CustomerAPI.java
+++ src/main/java/com/mafami/Mafami/api/CustomerAPI.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mafami.Mafami.Entity.CustomerEntity;
 import com.mafami.Mafami.Service.CustomerService;
 
 @RestController
+@RequestMapping("/api/customer")
 public class CustomerAPI {
 
 	@Autowired
