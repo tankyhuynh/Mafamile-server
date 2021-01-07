ff57f134d87614bb57bc8df5153d06fc19687fea
tankyhuynh <pokijellyperiod1@gmail.com>
2021-01-07 22:52:40 +0700

    Modify content

--- src/main/java/com/mafami/Mafami/Config/MongoConfig.java
+++ src/main/java/com/mafami/Mafami/Config/MongoConfig.java
@@ -1,37 +1,37 @@
-//package com.mafami.Mafami.Config;
-//
-//import java.util.Collection;
-//import java.util.Collections;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-//
-//import com.mongodb.ConnectionString;
-//import com.mongodb.MongoClientSettings;
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoClients;
-//
-//@Configuration
-//public class MongoConfig  extends AbstractMongoClientConfiguration {
-//
-//	@Override
-//    protected String getDatabaseName() {
-//        return "mafamile";
-//    }
-// 
-//    @Override
-//    public MongoClient mongoClient() {
-//        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
-//        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-//            .applyConnectionString(connectionString)
-//            .build();
-//        
-//        return MongoClients.create(mongoClientSettings);
-//    }
-// 
-//    @Override
-//    public Collection getMappingBasePackages() {
-//        return Collections.singleton("com.mafami.Mafami");
-//    }
-//	
-//}
+package com.mafami.Mafami.Config;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+public class MongoConfig  extends AbstractMongoClientConfiguration {
+
+	@Override
+    protected String getDatabaseName() {
+        return "mafamile";
+    }
+ 
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build();
+        
+        return MongoClients.create(mongoClientSettings);
+    }
+ 
+    @Override
+    public Collection getMappingBasePackages() {
+        return Collections.singleton("com.mafami.Mafami");
+    }
+	
+}
